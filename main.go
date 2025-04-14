9afdbee7909aa2fd53ce017246665d57358113d8
Dyego <dyego9444@gmail.com>
2025-04-14 19:38:18 -0300

    add sample table

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct{}
+
+type Coin struct {
+	Name   string
+	Symbol string
+	Price  float64
+}
 
 func (m model) Init() tea.Cmd {
 	return nil
@@ -35,5 +44,41 @@ func main() {
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error running program: %v\n", err)
 		return
+	}
+
+	columns := []table.Column{
+		{Title: "Name", Width: 10},
+		{Title: "Symbol", Width: 3},
+		{Title: "Price", Width: 5},
+	}
+
+	rows := []table.Row{
+		{"Bitcoin", "BTC", fmt.Sprintf("%.2f", 60000.0)},
+		{"Ethereum", "ETH", fmt.Sprintf("%.2f", 4000.0)},
+		{"Litecoin", "LTC", fmt.Sprintf("%.2f", 200.0)},
+	}
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+	)
+	t.SetHeight(10)
+	t.SetStyles(s)
+
+	m := model{t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
 	}
 }
